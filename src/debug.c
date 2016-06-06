ab779078f3982b935a3e0b42a2f792838f09dbb7
orenjiakira <orenjiakira@gmail.com>
2016-06-06 12:47:33 -0300

    Add logprint function

---
+++ src/debug.c
@@ -0,0 +1,17 @@
+#ifndef DEBUG_MODE
+
+void logprint(const char* fmt, ...) {}
+
+#else
+
+#include <stdio.h>
+#include <stdarg.h>
+
+void logprint(char* const fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+}
+
+#endif
---
+++ src/debug.h
@@ -0,0 +1,6 @@
+#ifndef GAME_DEBUG_H
+#define GAME_DEBUG_H
+
+void logprint(char* const fmt, ...);
+
+#endif
