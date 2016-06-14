568fa271ba55c73e84e58e4740e2a076aa9fe0bb
orenjiakira <orenjiakira@gmail.com>
2016-06-14 15:00:03 -0300

    Change controller writing to use macros

---
+++ src/controller.h
@@ -0,0 +1,49 @@
+#ifndef GAME_CONTROLLER_H
+#define GAME_CONTROLLER_H
+
+typedef void (*KeyHandler)();
+
+#define EVENT_ASSOCIATE(EVENT, KEY, FUNCTION) EVENT##_keyhandlers[KEY] = FUNCTION
+
+#define DECALRE_EVENTS(EVENT) static KeyHandler EVENT##_keyhandlers[KEYMAP_SIZE] = { \
+    __null_handler__, /* default */ \
+    __null_handler__, /* P1_MARU */ \
+    __null_handler__, /* P1_BATSU */ \
+    __null_handler__, /* P1_PAUSE */ \
+    __null_handler__, /* P1_UP */ \
+    __null_handler__, /* P1_DOWN */ \
+    __null_handler__, /* P1_LEFT */ \
+    __null_handler__, /* P1_RIGHT */ \
+    __null_handler__, /* P2_MARU */ \
+    __null_handler__, /* P2_BATSU */ \
+    __null_handler__, /* P2_PAUSE */ \
+    __null_handler__, /* P2_UP */ \
+    __null_handler__, /* P2_DOWN */ \
+    __null_handler__, /* P2_LEFT */ \
+    __null_handler__  /* P2_RIGHT */ }
+
+#define NEW_CONTROLLER(NAME) \
+    static bool init = false; \
+    static InputController NAME##Controller; \
+    static void __null_handler__() {} \
+    static void NAME##Controller_init(); \
+    DECALRE_EVENTS(press); \
+    DECALRE_EVENTS(release); \
+    DECALRE_EVENTS(hold); \
+    static void keypress(int key) { press_keyhandlers[key](); } \
+    static void keyrelease(int key) { release_keyhandlers[key](); } \
+    static void keyhold(int key) { hold_keyhandlers[key](); } \
+    void NAME##Controller_load() { \
+        if (!init) { NAME##Controller_init(); } \
+        NAME##Controller.keyPressed = keypress; \
+        NAME##Controller.keyReleased = keyrelease; \
+        NAME##Controller.keyHeld = keyhold; \
+        logprint("Stacking " #NAME " scene...\n"); \
+        Input_loadSceneController(&NAME##Controller); } \
+    static char *success_msg = "Controller" #NAME "initialized\n"
+
+/* Essa última linha de código serve para o uso
+da macro pedir o caracter ";" no fim da linha.
+Isso só faz meu syntax-highlighting não zuar huehue. */
+
+#endif
--- src/controllers/pressstart.c
+++ src/controllers/pressstart.c
@@ -2,63 +2,24 @@
 #include "debug.h"
 #include "sound.h"
 #include "input.h"
+#include "controller.h"
 #include "scenes/gameplay.h"
 #include "scenes/pressstart.h"
 #include "controllers/pressstart.h"
 
-static InputController controller;
+#include <stdbool.h>
 
-static void keypress(int key) {
-    switch (key) {
-        case P1_MARU:
-            Sound_playSE(FX_SELECT);
-            PressStart_close();
-            GamePlay_load();
-            break;
-        case P1_BATSU:
-            break;
-        case P1_PAUSE:
-            break;
-        case P1_UP:
-            break;
-        case P1_DOWN:
-            break;
-        case P1_LEFT:
-            break;
-        case P1_RIGHT:
-            break;
-        case P2_MARU:
-            Sound_playSE(FX_SELECT);
-            PressStart_close();
-            GamePlay_load();
-            break;
-        case P2_BATSU:
-            break;
-        case P2_PAUSE:
-            break;
-        case P2_UP:
-            break;
-        case P2_DOWN:
-            break;
-        case P2_LEFT:
-            break;
-        case P2_RIGHT:
-            break;
-        default:
-            break;
-    }
+/* Criamos um código pedante de mapeamento de controle usando macros */
+NEW_CONTROLLER(PressStart);
+
+static void press_any_shift() {
+    Sound_playSE(FX_SELECT);
+    PressStart_close();
+    GamePlay_load();
 }
 
-static void keyrelease(int key) {
-}
-
-static void keyhold(int key) {
-}
-
-void PressStartController_load() {
-    controller.keyPressed = keypress;
-    controller.keyReleased = keyrelease;
-    controller.keyHeld = keyhold;
-    logprint("Stacking press start scene...\n");
-    Input_loadSceneController(&controller);
+static void PressStartController_init() {
+    EVENT_ASSOCIATE(press, P1_MARU, press_any_shift);
+    EVENT_ASSOCIATE(press, P2_MARU, press_any_shift);
+    logprint(success_msg);
 }
