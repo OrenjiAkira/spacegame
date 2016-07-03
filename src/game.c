fa9358e57fcbce33d8fd1faa58bc92b2b4f8ebd1
orenjiakira <orenjiakira@gmail.com>
2016-07-03 00:51:34 -0300

    Add no music flag to avoid segfault

--- src/game.c
+++ src/game.c
@@ -29,8 +29,12 @@ static bool QUIT = false;
 static void Game_load() {
     Scene_load(SCENE_CHOOSEYOURCAT);
     logprint("UNITS: \n [ %f, %f ]\n", Map_getUnitX(), Map_getUnitY());
+    #ifndef NO_MUSIC
     Sound_playBGM();
     logprint("Playing music...\n");
+    #else
+    logprint("Not playing music...\n");
+    #endif
 }
 
 void Game_init(char const *execpath) {
