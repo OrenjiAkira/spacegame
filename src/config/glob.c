a4d60b849879a0f62615f90640462439c1fac3f4
orenjiakira <orenjiakira@gmail.com>
2016-07-02 21:22:05 -0300

    Add Config singleton

---
+++ src/config/glob.c
@@ -0,0 +1,19 @@
+
+#include "debug.h"
+#include "game.h"
+#include "config/glob.h"
+
+static int GLOBALS[GLOBAL_TOTAL];
+
+void Globals_init() {
+    /* Pedante */
+    int i; for (i = 0; i < GLOBAL_TOTAL; ++i) GLOBALS[i] = 0;
+}
+
+int Globals_get(int global_id) {
+    logprint("Requested global id #%d", global_id);
+    if (global_id < 0 || global_id >= GLOBAL_TOTAL) {
+        GAME_ERROR("INVALID GLOBAL ID REQUESTED. Preventing segmentation fault, but shutting down game.");
+        return 0;
+    } else return GLOBALS[global_id];
+}
